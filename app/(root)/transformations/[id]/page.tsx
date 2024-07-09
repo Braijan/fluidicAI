ad8aaa0f9e67cd7fca24e1d7c6a7e221a925fcf6
Braijan <brian@briancharlessmith.com>
2024-07-09 16:39:03 -0400

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TransformationsPage = () => {
+  return <div>TransformationsPage</div>;
+};
+
+export default TransformationsPage;
+
+// Not localhost:3000/transformations
+// localhost:3000/transformations/[id]
